44f88175e17a979132c7ef2766923380e12022ff
Florian Fromm <flrn.frmm@gmail.com>
2024-04-04 10:25:07 +0200

    chore(backend): refactored code
    Split pipleine code into multiple functions.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,40 @@
+use eyre::Result;
+use dagger_sdk::{Query, File, Container};
+
+
 #[tokio::main]
-async fn main() -> eyre::Result<()> {
-    // let client = dagger_sdk::connect().await?;
+async fn main() -> Result<()> {
+    let client = dagger_sdk::connect().await?;
+    let build = build_backend(&client).await;
+    let image = build_prod_image(&client, build).await;
+    let image_reference = push_image(image).await?;
+    println!("Image published at: {}", image_reference);
+    Ok(())
+}
 
-    // let backend_directory = client.host().directory("axum-backend");
+async fn build_backend(client: &Query) -> File {
+    let backend_directory = client.host().directory("axum-backend");
+    client
+        .container()
+        .from("rust:alpine")
+        .with_directory("./backend", backend_directory)
+        .with_workdir("/backend")
+        .with_exec(vec!["apk", "add", "build-base", "musl"])
+        .with_exec(vec!["cargo", "build", "--release"])
+        .file("./target/release/axum-backend")
+}
 
-    // let backend_build = client
-    //     .container()
-    //     .from("rust:alpine")
-    //     .with_directory("./backend", backend_directory)
-    //     .with_workdir("/backend")
-    //     .with_exec(vec!["apk", "add", "build-base", "musl"])
-    //     .with_exec(vec!["cargo", "build", "--release"])
-    //     .file("./target/release/axum-backend");
+async fn build_prod_image(client: &Query, build: File) -> Container {
+    client
+        .container()
+        .from("gcr.io/distroless/static-debian12")
+        .with_file(".", build)
+        .with_entrypoint(vec!["./axum-backend"])
+}
 
-    // let run_container = client
-    //     .container()
-    //     .from("gcr.io/distroless/static-debian12")
-    //     .with_file(".", backend_build)
-    //     .with_entrypoint(vec!["./axum-backend"]);
-
+async fn push_image(image: Container) -> Result<String> {
     let tag_uuid = uuid::Uuid::new_v4().to_string();
     let address = format!("ttl.sh/backend-{}", tag_uuid);
-    println!("Pushing container to {}", address);
-    // run_container.publish(address).await?;
-
-    Ok(())
+    let image_reference = image.publish(address).await?;
+    Ok(image_reference)
 }
